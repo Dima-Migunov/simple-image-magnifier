a83fbe816dec2552f16ed418de1804deb3143574
migunov <dmitri.migunov@gmail.com>
2025-02-16 11:49:20 +0300

    fix preview image

--- src/simple-image-magnifier.ts
+++ src/simple-image-magnifier.ts
@@ -1,65 +1,77 @@
 export default function SimpleImageMagnifier(
-    containerElement: string | HTMLElement,
-    imageElement: string | HTMLImageElement,
-    imageOriginalElement: string | HTMLImageElement
+  containerElement: string | HTMLElement,
+  imageElement: string | HTMLImageElement,
+  imageOriginalElement: string | HTMLImageElement
 ): () => void {
-    let zoomW: number = 1
-    let zoomH: number = 1
+  let zoomW: number = 1
+  let zoomH: number = 1
 
-    const previewBoxEl: HTMLElement | HTMLDivElement | null =
-        typeof containerElement == 'string' ? document.querySelector(containerElement) : containerElement
+  const previewBoxEl: HTMLElement | HTMLDivElement | null =
+    typeof containerElement == "string"
+      ? document.querySelector(containerElement)
+      : containerElement
 
-    if (!previewBoxEl) {
-        throw new Error('containerElement is not found')
-    }
+  if (!previewBoxEl) {
+    throw new Error("containerElement is not found")
+  }
 
-    const imageEl: HTMLImageElement | null =
-        typeof imageElement == 'string' ? previewBoxEl.querySelector(imageElement) : imageElement
+  const imageEl: HTMLImageElement | null =
+    typeof imageElement == "string"
+      ? previewBoxEl.querySelector(imageElement)
+      : imageElement
 
-    if (!imageEl) {
-        throw new Error('imageElement is not found')
-    }
+  if (!imageEl) {
+    throw new Error("imageElement is not found")
+  }
 
-    const imageOriginalEl: HTMLImageElement | null =
-        typeof imageOriginalElement == 'string'
-            ? previewBoxEl.querySelector(imageOriginalElement)
-            : imageOriginalElement
+  const imageOriginalEl: HTMLImageElement | null =
+    typeof imageOriginalElement == "string"
+      ? previewBoxEl.querySelector(imageOriginalElement)
+      : imageOriginalElement
 
+  if (!imageOriginalEl) {
+    throw new Error("imageOriginalEl is not found")
+  }
+
+  const refreshImage = () => {
     if (!imageOriginalEl) {
-        throw new Error('imageOriginalEl is not found')
+      return
     }
 
-    const refreshImage = () => {
-        if (!imageOriginalEl) {
-            return
-        }
+    imageOriginalEl.style.width = imageOriginalEl.naturalWidth + "px"
+    imageOriginalEl.style.height = imageOriginalEl.naturalHeight + "px"
 
-        imageOriginalEl.style.width = imageOriginalEl.naturalWidth + 'px'
-        imageOriginalEl.style.height = imageOriginalEl.naturalHeight + 'px'
+    zoomW = imageOriginalEl.naturalWidth / previewBoxEl.clientWidth - 1
+    zoomH = imageOriginalEl.naturalHeight / previewBoxEl.clientHeight - 1
+  }
 
-        zoomW = imageOriginalEl.naturalWidth / previewBoxEl.clientWidth - 1
-        zoomH = imageOriginalEl.naturalHeight / previewBoxEl.clientHeight - 1
-    }
+  const addEvents = () => {
+    imageOriginalEl.addEventListener("load", refreshImage)
 
-    const addEvents = () => {
-        imageOriginalEl.addEventListener('load', refreshImage)
+    previewBoxEl.addEventListener("mouseenter", () => {
+      imageEl.style.opacity = "0"
+    })
 
-        previewBoxEl.addEventListener('mouseenter', () => {
-            imageEl.style.opacity = '0'
-        })
+    previewBoxEl.addEventListener("mouseleave", () => {
+      imageEl.style.opacity = "1"
+    })
 
-        previewBoxEl.addEventListener('mouseleave', () => {
-            imageEl.style.opacity = '1'
-        })
+    previewBoxEl.addEventListener("mousemove", (e) => {
+      imageOriginalEl.style.top = -e.offsetY * zoomH + "px"
+      imageOriginalEl.style.left = -e.offsetX * zoomW + "px"
+    })
 
-        previewBoxEl.addEventListener('mousemove', (e) => {
-            imageOriginalEl.style.top = -e.offsetY * zoomH + 'px'
-            imageOriginalEl.style.left = -e.offsetX * zoomW + 'px'
-        })
-    }
+    imageEl.addEventListener("mouseenter", () => {
+      previewBoxEl.style.opacity = "0"
+    })
 
-    addEvents()
-    refreshImage()
+    imageEl.addEventListener("mouseleave", () => {
+      previewBoxEl.style.opacity = "1"
+    })
+  }
 
-    return refreshImage
+  addEvents()
+  refreshImage()
+
+  return refreshImage
 }
