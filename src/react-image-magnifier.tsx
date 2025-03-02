f3065b4cfd9beea7f1c61720a17a55632f6de8c8
migunov <dmitri.migunov@gmail.com>
2025-03-02 18:41:21 +0300

    optimization

--- src/react-image-magnifier.tsx
+++ src/react-image-magnifier.tsx
@@ -1,7 +1,7 @@
 "use client"
 
+import React, { useCallback, useEffect, useRef, useState } from "react"
 import ImageMagnifier from "./simple-image-magnifier"
-import React, { useEffect, useRef } from "react"
 
 type ObjectFit = "fill" | "contain" | "cover" | "none" | "scale-down"
 
@@ -22,30 +22,55 @@ const ReactImageMagnifier: React.FC<ReactImageMagnifierProps> = ({
   className = "",
   objectFit = "cover",
 }) => {
+  const [isOriginalLoaded, setIsOriginalLoaded] = useState<boolean>(false)
+
   const container = useRef<HTMLDivElement>(null)
   const imgPreview = useRef<HTMLImageElement>(null)
   const imgOriginal = useRef<HTMLImageElement>(null)
   const refreshImage = useRef<() => void>(() => {})
 
-  const showImageMagnifier = async () => {
-    if (container.current && imgPreview.current && imgOriginal.current) {
-      imgOriginal.current.onload = () => {
-        refreshImage.current = ImageMagnifier(
-          container.current as HTMLDivElement,
-          imgPreview.current as HTMLImageElement,
-          imgOriginal.current as HTMLImageElement
-        )
-      }
+  const handleOriginalImageLoad = useCallback(() => {
+    setIsOriginalLoaded(true)
+  }, [])
 
-      return
+  const handleOriginalImageError = useCallback(() => {
+    setIsOriginalLoaded(false)
+    console.error("Failed to load original image:", srcOriginal)
+  }, [srcOriginal])
+
+  const setupImageMagnifier = useCallback(() => {
+    if (
+      container.current &&
+      imgPreview.current &&
+      imgOriginal.current &&
+      isOriginalLoaded
+    ) {
+      refreshImage.current = ImageMagnifier(
+        container.current as HTMLDivElement,
+        imgPreview.current as HTMLImageElement,
+        imgOriginal.current as HTMLImageElement
+      )
     }
-
-    setTimeout(showImageMagnifier, 200)
-  }
+  }, [isOriginalLoaded])
 
   useEffect(() => {
-    showImageMagnifier()
-  }, [])
+    setupImageMagnifier()
+  }, [setupImageMagnifier])
+
+  useEffect(() => {
+    const originalImage = imgOriginal.current
+
+    if (originalImage) {
+      originalImage.addEventListener("load", handleOriginalImageLoad)
+      originalImage.addEventListener("error", handleOriginalImageError)
+    }
+    return () => {
+      if (originalImage) {
+        originalImage.removeEventListener("load", handleOriginalImageLoad)
+        originalImage.removeEventListener("error", handleOriginalImageError)
+      }
+    }
+  }, [handleOriginalImageLoad, handleOriginalImageError])
 
   useEffect(() => {
     refreshImage.current()
@@ -58,14 +83,14 @@ const ReactImageMagnifier: React.FC<ReactImageMagnifierProps> = ({
       style={{
         position: "relative",
         overflow: "hidden",
-        width: width ? width + "px" : "100%",
-        height: height ? height + "px" : "100%",
+        width: typeof width === "number" ? width + "px" : width ?? "100%",
+        height: typeof height === "number" ? height + "px" : width ?? "100%",
       }}
     >
       <img
         ref={imgPreview}
         src={srcPreview}
-        alt=""
+        alt="Preview"
         style={{
           position: "relative",
           zIndex: "1",
@@ -80,9 +105,25 @@ const ReactImageMagnifier: React.FC<ReactImageMagnifierProps> = ({
       <img
         ref={imgOriginal}
         src={srcOriginal}
-        alt=""
+        alt="Original"
         style={{ position: "absolute", maxWidth: "none", opacity: 0 }}
       />
+
+      {!isOriginalLoaded && (
+        <div
+          style={{
+            position: "absolute",
+            inset: 0,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            color: "#fff",
+            backgroundColor: "rgba(107 114 128 / 0.5)",
+          }}
+        >
+          &#9201;
+        </div>
+      )}
     </div>
   )
 }
--- src/simple-image-magnifier.ts
+++ src/simple-image-magnifier.ts
@@ -3,35 +3,38 @@ export default function SimpleImageMagnifier(
   imageElement: string | HTMLImageElement,
   imageOriginalElement: string | HTMLImageElement
 ): () => void {
+  // Helper function to get an element, throwing an error if not found.
+  const getElement = <T extends HTMLElement>(
+    selectorOrElement: string | T,
+    context?: HTMLElement
+  ): T => {
+    const element =
+      typeof selectorOrElement === "string"
+        ? (context || document).querySelector(selectorOrElement)
+        : selectorOrElement
+
+    if (!element) {
+      throw new Error(
+        `Element ${
+          typeof selectorOrElement === "string"
+            ? `with selector '${selectorOrElement}'`
+            : "is"
+        } not found`
+      )
+    }
+    return element as T
+  }
+
+  const previewBoxEl = getElement<HTMLElement>(containerElement)
+  const imageEl = getElement<HTMLImageElement>(imageElement, previewBoxEl)
+
+  const imageOriginalEl = getElement<HTMLImageElement>(
+    imageOriginalElement,
+    previewBoxEl
+  )
+
   let zoomW: number = 1
   let zoomH: number = 1
-
-  const previewBoxEl: HTMLElement | HTMLDivElement | null =
-    typeof containerElement == "string"
-      ? document.querySelector(containerElement)
-      : containerElement
-
-  if (!previewBoxEl) {
-    throw new Error("containerElement is not found")
-  }
-
-  const imageEl: HTMLImageElement | null =
-    typeof imageElement == "string"
-      ? previewBoxEl.querySelector(imageElement)
-      : imageElement
-
-  if (!imageEl) {
-    throw new Error("imageElement is not found")
-  }
-
-  const imageOriginalEl: HTMLImageElement | null =
-    typeof imageOriginalElement == "string"
-      ? previewBoxEl.querySelector(imageOriginalElement)
-      : imageOriginalElement
-
-  if (!imageOriginalEl) {
-    throw new Error("imageOriginalEl is not found")
-  }
 
   const refreshImage = () => {
     if (!imageOriginalEl) {
@@ -45,23 +48,26 @@ export default function SimpleImageMagnifier(
     zoomH = imageOriginalEl.naturalHeight / previewBoxEl.clientHeight - 1
   }
 
+  const handleMouseEnter = () => {
+    imageEl.style.opacity = "0"
+    imageOriginalEl.style.opacity = "1"
+  }
+
+  const handleMouseLeave = () => {
+    imageEl.style.opacity = "1"
+    imageOriginalEl.style.opacity = "0"
+  }
+
+  const handleMouseMove = (e: MouseEvent) => {
+    imageOriginalEl.style.top = `${-e.offsetY * zoomH}px`
+    imageOriginalEl.style.left = `${-e.offsetX * zoomW}px`
+  }
+
   const addEvents = () => {
     imageOriginalEl.addEventListener("load", refreshImage)
-
-    previewBoxEl.addEventListener("mouseenter", () => {
-      imageEl.style.opacity = "0"
-      imageOriginalEl.style.opacity = "1"
-    })
-
-    previewBoxEl.addEventListener("mouseleave", () => {
-      imageEl.style.opacity = "1"
-      imageOriginalEl.style.opacity = "0"
-    })
-
-    previewBoxEl.addEventListener("mousemove", (e) => {
-      imageOriginalEl.style.top = -e.offsetY * zoomH + "px"
-      imageOriginalEl.style.left = -e.offsetX * zoomW + "px"
-    })
+    previewBoxEl.addEventListener("mouseenter", handleMouseEnter)
+    previewBoxEl.addEventListener("mouseleave", handleMouseLeave)
+    previewBoxEl.addEventListener("mousemove", handleMouseMove)
   }
 
   addEvents()
